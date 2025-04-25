ad0c4fe0c54426c2e2cfa37f4f07727815cb1311
Roger-Melo <rogerw592@gmail.com>
2025-04-25 20:54:30 -0300

    add styles to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+
 export default function Home() {
   return (
-    <section className="bg-white">
-      <h1>Welcome to CodeLeap network!</h1>
-    </section>
+    <main className="min-h-screen flex flex-col justify-center p-4">
+      <section className="flex flex-col gap-4 bg-white border border-primary-gray rounded-2xl p-5 w-full sm:w-[500px] mx-auto">
+        <h1 className="text-lg sm:text-xl font-bold">Welcome to CodeLeap network!</h1>
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2 text-left">
+            <Label className="font-normal" htmlFor="username">Please enter your username</Label>
+            <Input required id="username" type="text" placeholder="John doe" autoFocus className="border-primary-darkest-gray" />
+          </div>
+          <Button type="submit" className="uppercase w-full py-5 bg-primary-blue sm:w-32 ml-auto hover:cursor-pointer hover:bg-primary-acqua">Enter</Button>
+        </form>
+      </section>
+    </main>
   )
 }
