ff2e1652527ed88f00214412cdc24a77cee1ba75
Roger-Melo <rogerw592@gmail.com>
2025-04-25 09:05:50 -0300

    add Roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Roboto } from "next/font/google"
 import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+const roboto = Roboto({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "CodeLeap Network",
@@ -22,7 +14,7 @@ type RootLayoutProps = Readonly<{ children: React.ReactNode }>
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${roboto.className} antialiased`}>
         {children}
       </body>
     </html>
