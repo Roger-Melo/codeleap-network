8a9f57570ff5dc67c144fe7ca74ef3e63cdb45eb
Roger-Melo <rogerw592@gmail.com>
2025-04-26 19:44:00 -0300

    create selected post handler functions

--- src/components/ui/posts-list.tsx
+++ src/components/ui/posts-list.tsx
@@ -8,10 +8,13 @@ import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, 
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, DialogClose } from "@/components/ui/dialog"
 import { usePostsContext } from "@/lib/hooks"
 
-function EditPost() {
+type PostIdProp = { postId: number }
+
+function EditPost({ postId }: PostIdProp) {
+  const { handleSelectPost, handleUnselectPost } = usePostsContext()
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <Dialog onOpenChange={(open) => !open && handleUnselectPost()}>
+      <DialogTrigger asChild onClick={() => handleSelectPost(postId)}>
         <svg className="w-5 h-5 sm:w-6 sm:h-6 fill-white hover:cursor-pointer hover:fill-white/80" viewBox="0 0 26 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M6.10094 18.2663L11.8385 18.2475L24.3614 6.3225C24.8528 5.85 25.1233 5.2225 25.1233 4.555C25.1233 3.8875 24.8528 3.26 24.3614 2.7875L22.2994 0.805C21.3165 -0.14 19.6016 -0.135 18.6265 0.80125L6.10094 12.7288V18.2663ZM20.461 2.5725L22.5269 4.55125L20.4506 6.52875L18.3886 4.5475L20.461 2.5725ZM8.70122 13.7713L16.5411 6.305L18.6031 8.2875L10.7645 15.7513L8.70122 15.7575V13.7713Z" />
           <path d="M3.50067 23.25H21.7026C23.1367 23.25 24.3029 22.1287 24.3029 20.75V9.91499L21.7026 12.415V20.75H7.6065C7.5727 20.75 7.5376 20.7625 7.50379 20.7625C7.46089 20.7625 7.41798 20.7512 7.37378 20.75H3.50067V3.24999H12.4027L15.003 0.749994H3.50067C2.06661 0.749994 0.900391 1.87124 0.900391 3.24999V20.75C0.900391 22.1287 2.06661 23.25 3.50067 23.25Z" />
@@ -43,10 +46,11 @@ function EditPost() {
   )
 }
 
-function DeletePost() {
+function DeletePost({ postId }: PostIdProp) {
+  const { handleSelectPost, handleUnselectPost } = usePostsContext()
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
+    <AlertDialog onOpenChange={(open) => !open && handleUnselectPost()}>
+      <AlertDialogTrigger asChild onClick={() => handleSelectPost(postId)}>
         <svg className="w-4 h-5 sm:w-5 sm:h-6 fill-white hover:cursor-pointer hover:fill-white/80" viewBox="0 0 19 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M1.80087 20.75C1.80087 22.125 2.971 23.25 4.40115 23.25H14.8023C16.2324 23.25 17.4025 22.125 17.4025 20.75V5.75H1.80087V20.75ZM4.99921 11.85L6.83241 10.0875L9.6017 12.7375L12.358 10.0875L14.1912 11.85L11.4349 14.5L14.1912 17.15L12.358 18.9125L9.6017 16.2625L6.84541 18.9125L5.01221 17.15L7.76851 14.5L4.99921 11.85ZM14.1522 2L12.852 0.75H6.35136L5.05122 2H0.500732V4.5H18.7027V2H14.1522Z" />
         </svg>
@@ -80,8 +84,8 @@ export function PostsList() {
                 <h3 className="text-2xl sm:text-2xl font-bold text-white">{post.title}</h3>
                 <nav className="sm:flex sm:flex-col sm:justify-center">
                   <ul className="flex gap-4 sm:gap-6 justify-end">
-                    <li><EditPost /></li>
-                    <li><DeletePost /></li>
+                    <li><EditPost postId={post.id} /></li>
+                    <li><DeletePost postId={post.id} /></li>
                   </ul>
                 </nav>
               </header>
--- src/contexts/posts-context-provider.tsx
+++ src/contexts/posts-context-provider.tsx
@@ -6,6 +6,8 @@ import { type Post } from "@/lib/types"
 type PostsContextType = {
   posts: Post[]
   selectedPostId: number | null
+  handleSelectPost: (id: number) => void
+  handleUnselectPost: () => void
 }
 
 type PostsContextProviderProps = {
@@ -17,9 +19,18 @@ export const PostsContext = createContext<PostsContextType | null>(null)
 
 export function PostsContextProvider({ data, children }: PostsContextProviderProps) {
   const [posts, setPosts] = useState(data)
-  const [selectedPostId, setSelectedPostId] = useState(null)
+  const [selectedPostId, setSelectedPostId] = useState<number | null>(null)
+
+  function handleSelectPost(id: number) {
+    setSelectedPostId(id)
+  }
+
+  function handleUnselectPost() {
+    setSelectedPostId(null)
+  }
+
   return (
-    <PostsContext.Provider value={{ posts, selectedPostId }}>
+    <PostsContext.Provider value={{ posts, selectedPostId, handleSelectPost, handleUnselectPost }}>
       {children}
     </PostsContext.Provider>
   )
