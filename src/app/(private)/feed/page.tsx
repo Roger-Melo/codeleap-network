43e2740b57bc5f1d81819ef0560caba6b8e46c78
Roger-Melo <rogerw592@gmail.com>
2025-04-26 08:26:59 -0300

    create Header component

--- src/app/(private)/feed/page.tsx
+++ src/app/(private)/feed/page.tsx
@@ -108,13 +108,18 @@ function PostForm() {
   )
 }
 
+function Header() {
+  return (
+    <header className="bg-primary-blue py-8 px-6 border-b border-primary-dark-gray">
+      <h1 className="text-xl sm:text-2xl font-bold text-white">CodeLeap Network</h1>
+    </header>
+  )
+}
+
 export default function Feed() {
   return (
     <section className="bg-white mx-auto max-w-[800px] min-h-screen">
-      <header className="bg-primary-blue py-8 px-6 border-b border-primary-dark-gray">
-        <h1 className="text-xl sm:text-2xl font-bold text-white">CodeLeap Network</h1>
-      </header>
-
+      <Header />
       <main className="p-6 flex flex-col gap-6">
         <section className="border border-primary-dark-gray rounded-2xl p-5 flex flex-col gap-2">
           <h2 className="text-lg sm:text-xl font-bold">What’s on your mind?</h2>
