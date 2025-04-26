ad52f1432baf427b896450d7364d8f8ff208b705
Roger-Melo <rogerw592@gmail.com>
2025-04-26 08:24:24 -0300

    create PostForm component

--- src/app/(private)/feed/page.tsx
+++ src/app/(private)/feed/page.tsx
@@ -92,6 +92,22 @@ function Post() {
   )
 }
 
+function PostForm() {
+  return (
+    <form className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2 text-left">
+        <Label className="font-normal" htmlFor="title">Title</Label>
+        <Input required id="title" type="text" placeholder="Hello world" autoFocus className="border-primary-darkest-gray" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label className="font-normal" htmlFor="content">Content</Label>
+        <Textarea required id="content" placeholder="Content here" className="border-primary-darkest-gray" />
+      </div>
+      <Button type="submit" className="bg-primary-blue ml-auto hover:bg-primary-acqua">Create</Button>
+    </form>
+  )
+}
+
 export default function Feed() {
   return (
     <section className="bg-white mx-auto max-w-[800px] min-h-screen">
@@ -102,18 +118,7 @@ export default function Feed() {
       <main className="p-6 flex flex-col gap-6">
         <section className="border border-primary-dark-gray rounded-2xl p-5 flex flex-col gap-2">
           <h2 className="text-lg sm:text-xl font-bold">What’s on your mind?</h2>
-
-          <form className="flex flex-col gap-4">
-            <div className="flex flex-col gap-2 text-left">
-              <Label className="font-normal" htmlFor="title">Title</Label>
-              <Input required id="title" type="text" placeholder="Hello world" autoFocus className="border-primary-darkest-gray" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label className="font-normal" htmlFor="content">Content</Label>
-              <Textarea required id="content" placeholder="Content here" className="border-primary-darkest-gray" />
-            </div>
-            <Button type="submit" className="bg-primary-blue ml-auto hover:bg-primary-acqua">Create</Button>
-          </form>
+          <PostForm />
         </section>
         <Post />
       </main>
