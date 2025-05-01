0080bfaaca7b6e9e0285a6cc5b479bed5b8a6456
Roger-Melo <rogerw592@gmail.com>
2025-05-01 13:51:27 -0300

    create useUsernameContext hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,10 +1,20 @@
 import { useContext } from "react"
 import { PostsContext } from "@/contexts/posts-context-provider"
+import { UsernameContext } from "@/contexts/username-context-provider"
 
 export function usePostsContext() {
   const context = useContext(PostsContext)
   if (!context) {
     throw new Error("useContext must be used within a PostsContextProvider")
+  }
+
+  return context
+}
+
+export function useUsernameContext() {
+  const context = useContext(UsernameContext)
+  if (!context) {
+    throw new Error("useContext must be used within a UsernameContextProvider")
   }
 
   return context
