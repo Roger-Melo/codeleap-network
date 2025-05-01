4e15ddf5756b9348c67fa1f9d76b4189bf895031
Roger-Melo <rogerw592@gmail.com>
2025-05-01 16:20:16 -0300

    using ClientProtection component

--- src/app/(private)/feed/layout.tsx
+++ src/app/(private)/feed/layout.tsx
@@ -1,11 +1,12 @@
 import { postsDataSchema } from "@/lib/types"
 import { PostsContextProvider } from "@/contexts/posts-context-provider"
 import { baseUrl } from "@/lib/utils"
+import { ClientProtection } from "@/components/client-protection"
 
-function ErrorMessage() {
+function PageErrorMessage() {
   return (
     <h2 className="text-center mx-auto text-primary-red">
-      Could not get posts data. Please try again in a few minutes.
+      Could not get the page. Please try again in a few minutes.
     </h2>
   )
 }
@@ -15,12 +16,18 @@ type FeedLayoutProps = { children: React.ReactNode }
 export default async function FeedLayout({ children }: FeedLayoutProps) {
   const response = await fetch(baseUrl)
   if (!response.ok) {
-    return <ErrorMessage />
+    return <PageErrorMessage />
   }
 
   const data: unknown = await response.json()
   const validatedData = postsDataSchema.safeParse(data)
   return validatedData.success
-    ? <PostsContextProvider data={validatedData.data.results}>{children}</PostsContextProvider>
-    : <ErrorMessage />
+    ? (
+      <PostsContextProvider data={validatedData.data.results}>
+        <ClientProtection>
+          {children}
+        </ClientProtection>
+      </PostsContextProvider>
+    )
+    : <PageErrorMessage />
 }
