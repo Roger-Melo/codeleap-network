35af205036b4e402ab8e984577b464d9f93fca22
Roger-Melo <rogerw592@gmail.com>
2025-05-01 10:58:36 -0300

    add initial LoginForm implementation

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,14 +1,23 @@
+"use client"
+
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 
 export function LoginForm() {
+  const [usernameFieldState, setUsernameFieldState] = useState("")
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUsernameFieldState(e.target.value)
+
   return (
     <form className="flex flex-col gap-4">
       <div className="flex flex-col gap-2 text-left">
         <Label className="font-normal" htmlFor="username">Please enter your username</Label>
         <Input
-          required
+          value={usernameFieldState}
+          onChange={handleUsernameChange}
           id="username"
           type="text"
           placeholder="John doe"
@@ -16,7 +25,11 @@ export function LoginForm() {
           className="border-primary-darkest-gray"
         />
       </div>
-      <Button type="submit" className="uppercase bg-primary-blue ml-auto hover:bg-primary-acqua">
+      <Button
+        disabled={usernameFieldState === ""}
+        type="submit"
+        className="uppercase bg-primary-blue ml-auto hover:bg-primary-acqua"
+      >
         Enter
       </Button>
     </form>
