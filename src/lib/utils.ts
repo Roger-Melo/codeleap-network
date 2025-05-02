daae824156592713f1f543489a6ad296e826e0a9
Roger-Melo <rogerw592@gmail.com>
2025-05-02 11:00:47 -0300

    create getPostTimeCreation function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,14 +11,30 @@ export async function delay(milliseconds: number) {
   await new Promise((resolve) => setTimeout(resolve, milliseconds))
 }
 
-export function generateTempTimestamp() {
-  const now = new Date()
-  const iso = now.toISOString()
-  const [date, time] = iso.split('T')
-  const [hms, msAndZ] = time.split('.')
-  const ms = msAndZ.slice(0, 3) // milliseconds part
-  const extraMicroseconds = String(Math.floor(Math.random() * 1000)).padStart(3, '0') // fake microseconds
-  return `${date}T${hms}.${ms}${extraMicroseconds}Z`
+export function getPostTimeCreation(timestamp: string) {
+  const nowMiliseconds = Date.now()
+  const createdMiliseconds = new Date(timestamp).getTime()
+  const seconds = (nowMiliseconds - createdMiliseconds) / 1000
+
+  if (seconds < 60) {
+    return "just now"
+  }
+
+  const minutes = seconds / 60
+  if (minutes < 60) {
+    const m = Math.floor(minutes)
+    return `${m} minute${m !== 1 ? "s" : ""} ago`
+  }
+
+  const hours = minutes / 60
+  if (hours < 24) {
+    const h = Math.floor(hours)
+    return `${h} hour${h !== 1 ? "s" : ""} ago`
+  }
+
+  const days = hours / 24
+  const d = Math.floor(days)
+  return `${d} day${d !== 1 ? "s" : ""} ago`
 }
 
 export function alertIfError(error: { message: string } | undefined) {
