93dccd8b1dc6e0baba5fd42e49e2e79e753f676e
Roger-Melo <rogerw592@gmail.com>
2025-05-03 14:16:30 -0300

    e2e: add post creation test

--- tests/crud.spec.ts
+++ tests/crud.spec.ts
@@ -1,15 +1,37 @@
+import { type PostFormType } from "@/lib/types"
 import { test, expect, type Page } from "@playwright/test"
 
-const firstPost = {
-  title: "Post 1",
-  message: `Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
-
-Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat.`
+type CommonPostProps = {
+  post: PostFormType
+  page: Page
 }
+
+const posts = [
+  { title: "Post 1", content: "Parágrafo 1 do post 1.\n\nParágrafo 2 do post 1." },
+  { title: "Post 2", content: "Parágrafo 1 do post 2.\n\nParágrafo 2 do post 2." },
+  { title: "Post 3", content: "Parágrafo 1 do post 3.\n\nParágrafo 2 do post 3." },
+] as const
 
 async function awaitFiveSeconds(page: Page) {
   // necessary to skip the optimistic UI test & test the real outcome confirmed by the server
   await page.waitForTimeout(5_000)
+}
+
+async function createPost({ post, page }: CommonPostProps) {
+  const inputTitle = page.getByLabel("Title")
+  const textAreaContent = page.getByLabel("Content")
+  const buttonCreatePost = page.getByRole("button", { name: "Create" })
+  await inputTitle.fill(post.title)
+  await textAreaContent.fill(post.content)
+  await buttonCreatePost.click()
+}
+
+async function checkCreatedPost({ post, page }: CommonPostProps) {
+  await awaitFiveSeconds(page)
+  await expect(page.getByRole("heading", { name: post.title })).toBeVisible()
+  const firstPostParagraphs = post.content.split("\n\n")
+  await expect(page.getByRole("listitem").filter({ hasText: firstPostParagraphs[0] })).toBeVisible()
+  await expect(page.getByRole("listitem").filter({ hasText: firstPostParagraphs[1] })).toBeVisible()
 }
 
 test.beforeEach("Access /feed", async ({ page }) => {
@@ -22,16 +44,9 @@ test.beforeEach("Access /feed", async ({ page }) => {
 
 test.describe("New post", () => {
   test("Create posts on posts list", async ({ page }) => {
-    const inputTitle = page.getByLabel("Title")
-    const textAreaContent = page.getByLabel("Content")
-    const buttonCreatePost = page.getByRole("button", { name: "Create" })
-
-    await inputTitle.fill(firstPost.title)
-    await textAreaContent.fill(firstPost.message)
-    await buttonCreatePost.click()
-
-    const firstPostTitle = page.getByRole("heading", { name: firstPost.title })
-    await awaitFiveSeconds(page)
-    await expect(firstPostTitle).toBeVisible()
+    for (const post of posts) {
+      await createPost({ post, page })
+      await checkCreatedPost({ post, page })
+    }
   })
 })
