c1b339ab8885119abf27703af02f20ce85123c26
Roger-Melo <rogerw592@gmail.com>
2025-05-03 12:17:46 -0300

    e2e - wip: writing create posts test

--- tests/crud.spec.ts
+++ tests/crud.spec.ts
@@ -1,8 +1,16 @@
-import { test, expect } from "@playwright/test"
+import { test, expect, type Page } from "@playwright/test"
 
-const exampleText = `Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
+const firstPost = {
+  title: "Post 1",
+  message: `Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
 
 Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat.`
+}
+
+async function awaitFiveSeconds(page: Page) {
+  // necessary to skip the optimistic UI test & test the real outcome confirmed by the server
+  await page.waitForTimeout(5_000)
+}
 
 test.beforeEach("Access /feed", async ({ page }) => {
   await page.goto("/")
@@ -18,8 +26,12 @@ test.describe("New post", () => {
     const textAreaContent = page.getByLabel("Content")
     const buttonCreatePost = page.getByRole("button", { name: "Create" })
 
-    await inputTitle.fill("post 1")
-    await textAreaContent.fill(exampleText)
+    await inputTitle.fill(firstPost.title)
+    await textAreaContent.fill(firstPost.message)
     await buttonCreatePost.click()
+
+    const firstPostTitle = page.getByRole("heading", { name: firstPost.title })
+    await awaitFiveSeconds(page)
+    await expect(firstPostTitle).toBeVisible()
   })
 })
