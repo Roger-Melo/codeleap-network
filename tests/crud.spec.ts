755bbc0baef593143ed9dfd6967399d21c7b6d61
Roger-Melo <rogerw592@gmail.com>
2025-05-03 17:30:54 -0300

    e2e: add post deletion test

--- tests/crud.spec.ts
+++ tests/crud.spec.ts
@@ -1,11 +1,6 @@
 import { type PostFormType } from "@/lib/types"
 import { test, expect, type Page } from "@playwright/test"
 
-type CommonPostProps = {
-  post: PostFormType
-  page: Page
-}
-
 const posts = [
   { title: "Post 1", content: "Parágrafo 1 do post 1.\n\nParágrafo 2 do post 1." },
   { title: "Post 2", content: "Parágrafo 1 do post 2.\n\nParágrafo 2 do post 2." },
@@ -17,6 +12,23 @@ async function awaitFiveSeconds(page: Page) {
   await page.waitForTimeout(5_000)
 }
 
+async function deletePosts(page: Page) {
+  for (const post of posts) {
+    const selectedPost = page.getByRole("listitem").filter({ hasText: post.title })
+    await selectedPost.getByTestId("deletePost").click()
+    const deleteButton = page.getByRole("button", { name: "Delete" })
+    deleteButton.click()
+  }
+  await checkDeletedPosts(page)
+}
+
+async function checkDeletedPosts(page: Page) {
+  await awaitFiveSeconds(page)
+  for (const post of posts) {
+    await expect(page.locator("main")).not.toHaveText(post.title)
+  }
+}
+
 async function createPosts(page: Page) {
   const inputTitle = page.getByLabel("Title")
   const textAreaContent = page.getByLabel("Content")
@@ -26,9 +38,11 @@ async function createPosts(page: Page) {
     await textAreaContent.fill(post.content)
     await buttonCreatePost.click()
   }
+  await checkCreatedPosts(page)
 }
 
 async function checkCreatedPosts(page: Page) {
+  await awaitFiveSeconds(page)
   for (const post of posts) {
     const postTitle = page.getByRole("heading", { name: post.title })
     await expect(postTitle).toBeVisible()
@@ -49,44 +63,12 @@ test.beforeEach("Access /feed", async ({ page }) => {
 test.describe("Post creation", () => {
   test("Create posts on posts list", async ({ page }) => {
     await createPosts(page)
-    await awaitFiveSeconds(page)
-    await checkCreatedPosts(page)
   })
 })
-
-/*
-async function deletePost({ post, page }: CommonPostProps) {
-  const selectedPost = page.getByRole("listitem").filter({ hasText: post.title })
-  await selectedPost.getByTestId("deletePost").click()
-  const deleteButton = page.getByRole("button", { name: "Delete" })
-  deleteButton.click()
-return selectedPost
-}
-
-async function checkDeletedPost({ selectedPost, page }: { selectedPost: Locator, page: Page }) {
-  await awaitFiveSeconds(page)
-  await expect(selectedPost).not.toBeVisible()
-}
 
 test.describe("Post deletion", () => {
   test("Delete posts from posts list", async ({ page }) => {
-    for (const post of posts) {
-      await createPost({ post, page })
-    }
-    await awaitFiveSeconds(page)
-    await checkCreatedPost({ post: posts[0], page })
-    await checkCreatedPost({ post: posts[1], page })
-    await checkCreatedPost({ post: posts[2], page })
-
-    for (const post of posts) {
-      await deletePost({ post, page })
-const selectedPost = await deletePost({ post, page })
-await checkDeletedPost({ selectedPost, page })
-    }
-    await awaitFiveSeconds(page)
-    await expect(page.getByRole("listitem")).not.toHaveText(posts[0].title)
-    await expect(page.getByRole("listitem")).not.toHaveText(posts[1].title)
-    await expect(page.getByRole("listitem")).not.toHaveText(posts[2].title)
+    await createPosts(page)
+    await deletePosts(page)
   })
 })
-*/
